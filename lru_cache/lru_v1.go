53644c7659fc86f3517245358f981d5737fe0612
GeekGhc <ghcgavin@sina.com>
2020-11-10 00:59:02 +0800

    cache struct init

--- lru_cache/lru_v1.go
+++ lru_cache/lru_v1.go
@@ -1,1 +1,31 @@
 package lru_cache
+
+import (
+	"container/list"
+	"sync"
+)
+
+type value struct {
+	data   []byte
+	lruPos *list.Element
+}
+
+type lruCache struct {
+	maxSize int
+	data    map[interface{}]*value
+	lck     *sync.Mutex
+	lru     *list.List
+}
+
+//lru init
+func NewLruCache(maxLength int) *lruCache {
+	if maxLength <= 0 {
+		return nil
+	}
+	return &lruCache{
+		maxSize: maxLength,
+		data:    make(map[interface{}]*value),
+		lck:     new(sync.Mutex),
+		lru:     list.New(),
+	}
+}
