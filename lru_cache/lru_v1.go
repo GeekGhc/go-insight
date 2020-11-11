aeda326ac4f735a0dca83e914e1d5fbc11dc6d5b
GeekGhc <ghcgavin@sina.com>
2020-11-12 00:04:17 +0800

    lru set

--- lru_cache/lru_v1.go
+++ lru_cache/lru_v1.go
@@ -2,32 +2,55 @@ package lru_cache
 
 import (
 	"container/list"
-	"sync"
 )
 
 type elem struct {
-	data   []byte
-	lruPos *list.Element
+	lruPos *list.Element //list element
 }
 
-type lruCache struct {
-	maxSize   int
-	elemCount int
-	data      map[interface{}]*elem //hash表
-	lck       *sync.Mutex
-	lru       *list.List //节点链表
+type LruCache struct {
+	capacity int
+	length   int
+	dataMap  map[interface{}]*elem //hash表
+	lru      *list.List            //节点链表
 }
 
 //lru init
-func NewLruCache(maxLength int) *lruCache {
+func NewLruCache(maxLength int) *LruCache {
 	if maxLength <= 0 {
 		return nil
 	}
-	return &lruCache{
-		maxSize:   maxLength,
-		elemCount: 0,
-		data:      make(map[interface{}]*elem),
-		lck:       new(sync.Mutex),
-		lru:       list.New(),
+	return &LruCache{
+		capacity: maxLength,
+		length:   0,
+		dataMap:  make(map[interface{}]*elem),
+		lru:      list.New(),
 	}
+}
+
+func (c *LruCache) Set(key, value interface{}) {
+	if v, ok := c.dataMap[key]; ok {
+		//删除原先list的位置
+		c.deleteLruItem(v.lruPos)
+		//更新到list头部
+		v.lruPos = c.syncLruItem(key)
+		c.dataMap[key] = v
+	}
+}
+
+func (c *LruCache) deleteLruItem(pos *list.Element) {
+	//删除list
+	c.lru.Remove(pos)
+	//删除hashMap
+	delete(c.dataMap, pos.Value)
+	if c.length >= 0 {
+		c.length--
+	}
+	return
+}
+
+func (c *LruCache) syncLruItem(key interface{}) (item *list.Element) {
+	item = c.lru.PushFront(key)
+	c.length++
+	return
 }
